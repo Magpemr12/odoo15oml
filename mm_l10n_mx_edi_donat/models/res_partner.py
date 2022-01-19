92e42ec6b87139a90276b6f7160606a155a5f98e
unknown <traceur.rick@gmail.com>
2022-01-19 12:29:07 -0600

    Modificación al módulo de donativos

--- mm_l10n_mx_edi_donat/models/res_partner.py
+++ mm_l10n_mx_edi_donat/models/res_partner.py
@@ -5,10 +5,4 @@ from odoo import fields, models
 class ResPartner(models.Model):
     _inherit = 'res.partner'
 
-    l10n_mx_edi_donations = fields.Boolean(
-        'Need donations?',
-        help='Use this field when the invoice require the complement to '
-        '"Donations". This value will be used to indicate the use of the '
-        'information from the document that authorize to receive '
-        'deductible donations, granted by SAT')
     generic_customer = fields.Boolean("Generic Customer")
